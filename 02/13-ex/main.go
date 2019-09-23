0f1888e18630556a9b89ab38f0281cefad41462c
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-23 00:06:50 -0700

    updated code for ex 13

--- 02/13-ex/main.go
+++ 02/13-ex/main.go
@@ -4,25 +4,6 @@ import "fmt"
 
 func main() {
 
-	// You can check the Go Environment by typing `go env` on terminal
-
-	// You can create executables for different platform as follows:
-
-	// Create an OS X executable (from linux/unix terminal):
-	// GOOS=darwin GOARCH=386 go build
-
-	// Create a Windows executable (from linux/unix terminal):
-	// GOOS=windows GOARCH=386 go build
-
-	// Create a Linux executable (from linux/unix terminal):
-	// GOOS=linux GOARCH=arm GOARM=7 go build
-
-	// You can find the full list in here:
-	// https://golang.org/doc/install/source#environment
-
-	// 😱NOTE: If you're using the command prompt or the powershell, you may need to use it like this:
-	// cmd /c "set GOOS=darwin GOARCH=386 && go build"
-
 	fmt.Println("My name: Sumit S")
 	fmt.Println("My friend's name: Friend")
 
@@ -37,6 +18,7 @@ func main() {
 
 	// Gives Error
 	// fmt.Println('single Quotes don\'t work either')
+
 }
 
 // Importing here gives error.
