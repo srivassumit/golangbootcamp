6710df16a99ae6e439e47ec70de50cd5a8d1d324
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-23 23:03:13 -0700

    code for section 4 25 statements

---
+++ 04/25-statements/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  // Ctrl-K + Crl-Shift-S -> save without formatting
+  // Saving autmatically formats below lines in VS Code.
+	fmt.Println("Hello!"); fmt.Println("Bye!")
+
+	//  execution flow
+	if 5 > 1 {
+		fmt.Println("bigger")
+	}
+}
