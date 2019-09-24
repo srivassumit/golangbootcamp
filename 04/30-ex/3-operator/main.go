cc4c304dc6806dc4fe36688bcd4821af6249abdc
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-23 23:21:24 -0700

    Code for Section 4 Ex-3

---
+++ 04/30-ex/3-operator/main.go
@@ -0,0 +1,27 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Operators combine the expressions
+//
+//  Print the expected output below using the string
+//  concatenation operator.
+//
+// HINT
+//  Use + operator multiple times to create "Hello!!!?".
+//
+// EXPECTED OUTPUT
+//  "Hello!!!?"
+// ---------------------------------------------------------
+
+func main() {
+	fmt.Println("Hello!" + "!!?")
+}
