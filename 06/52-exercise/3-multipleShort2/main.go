ab288b01507605e324a554e6e01dff21daa02847
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-28 16:27:06 -0700

    Code for Section 6 52 Ex-3

---
+++ 06/52-exercise/3-multipleShort2/main.go
@@ -0,0 +1,30 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Multiple Short Declare #2
+//
+// 	1. Declare two variables using short declaration
+//
+//  2. `a` variable's value should be 42
+//  3. `c` variable's value should be "good"
+//
+// EXPECTED OUTPUT
+//  42 good
+// ---------------------------------------------------------
+
+func main() {
+	// ADD YOUR DECLARATIONS HERE
+	a, c := 42, "good"
+
+	// THEN UNCOMMENT THE CODE BELOW
+	fmt.Println(a, c)
+}
