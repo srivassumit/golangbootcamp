ba9b078669e2705aaea2aa415efdd4ab814d2323
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-28 16:27:16 -0700

    Code for Section 6 52 Ex-5

---
+++ 06/52-exercise/5-shortDiscard/main.go
@@ -0,0 +1,38 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Short Discard
+//
+// 	1. Short declare two bool variables
+//     (use multiple short declaration syntax)
+//
+//  2. Initialize both variables to true
+//
+//  3. Change your declaration and
+//     discard the 2nd variable's value
+//     using the blank-identifier
+//
+//  4. Print only the 1st variable
+//
+// EXPECTED OUTPUT
+//  true
+// ---------------------------------------------------------
+
+func main() {
+	// ADD YOUR DECLARATIONS HERE
+	on, off := true, true
+
+	_ = off
+
+	// THEN UNCOMMENT THE CODE BELOW
+	fmt.Println(on)
+}
