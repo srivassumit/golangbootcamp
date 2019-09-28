68737a175d6fb0b1de6e99ac2ef990a1221f5a60
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-28 15:15:15 -0700

    Code for Section 6 46 Ex-3

---
+++ 06/46-exercise/3-float64/main.go
@@ -0,0 +1,26 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Declare float64
+//
+//  1. Declare and print a variable with a float64 type
+//
+//  2. The declared variable's name should be: brightness
+//
+// EXPECTED OUTPUT
+//  0
+// ---------------------------------------------------------
+
+func main() {
+	var brightness float64
+	fmt.Println(brightness)
+}
