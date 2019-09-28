fae9af4258f1fc685308d6a9e40e308b601a40a1
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-28 15:15:24 -0700

    Code for Section 6 46 Ex-5

---
+++ 06/46-exercise/5-undeclarables/main.go
@@ -0,0 +1,34 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+// ---------------------------------------------------------
+// EXERCISE: Undeclarables
+//
+//  1. Declare the variables below:
+//      3speed
+//      !speed
+//      spe?ed
+//      var
+//      func
+//      package
+//
+//  2. Observe the error messages
+//
+// NOTE
+//  The types of the variables are not important.
+// ---------------------------------------------------------
+
+func main() {
+	// var 3speed int
+	// var !speed int
+	// var spe?ed int
+	// var var int
+	// var func int
+	// var package int
+}
