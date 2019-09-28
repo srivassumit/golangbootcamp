1f22d76bb71c555a497aee018b40eb48205b81a1
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-28 16:27:02 -0700

    Code for Section 6 52 Ex-2

---
+++ 06/52-exercise/2-multipleshort/main.go
@@ -0,0 +1,27 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Multiple Short Declare
+//
+//  Declare two variables using multiple short declaration
+//
+// EXPECTED OUTPUT
+//  14 true
+// ---------------------------------------------------------
+
+func main() {
+	// ADD YOUR DECLARATIONS HERE
+	a, b := 14, true
+
+	// THEN UNCOMMENT THE CODE BELOW
+	fmt.Println(a, b)
+}
