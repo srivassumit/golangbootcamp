609b732982adedf25e51bdea74f401977d947190
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-28 16:01:51 -0700

    Code for Section 6 48 Package short declaration

---
+++ 06/48-packageshortDeclaration/main.go
@@ -0,0 +1,13 @@
+package main
+
+// This won't work at the package level
+// safe:= true
+
+// This works at the pacakge level
+var safe = true
+
+// This is because all the statements at package level start with a keyword
+
+func main() {
+
+}
