2df72e748099f17aa3613eef497c4d866574c919
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-28 15:15:10 -0700

    Code for Section 6 46 Ex-2

---
+++ 06/46-exercise/2-bool/main.go
@@ -0,0 +1,26 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Declare bool
+//
+//  1. Declare and print a bool variable
+//
+//  2. The variable's name should be: isOn
+//
+// EXPECTED OUTPUT
+//  false
+// ---------------------------------------------------------
+
+func main() {
+	var isOn bool
+	fmt.Println(isOn)
+}
