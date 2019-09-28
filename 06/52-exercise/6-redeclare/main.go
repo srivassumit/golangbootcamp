43a0d9f6699af8f001aa1c090e7bfb4963f03580
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-28 16:28:43 -0700

    Code for Section 6 52 Ex-6

---
+++ 06/52-exercise/6-redeclare/main.go
@@ -0,0 +1,36 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Redeclare
+//
+// 	1. Short declare two int variables: age and yourAge
+//     (use multiple short declaration syntax)
+//
+//  2. Short declare a new float variable: ratio
+//     And, change the 'age' variable to 42
+//
+//     (! You should use redeclaration)
+//
+//  4. Print all the variables
+//
+// EXPECTED OUTPUT
+//  42, 20, 3.14
+// ---------------------------------------------------------
+
+func main() {
+	// ADD YOUR DECLARATIONS HERE
+	age, yourAge := 12, 20
+	age, ratio := 42, 3.14
+
+	// THEN UNCOMMENT THE CODE BELOW
+	fmt.Println(age, yourAge, ratio)
+}
