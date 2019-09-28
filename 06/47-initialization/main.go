b738a34336ecf335eac35b488aed86d73f122100
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-28 15:23:55 -0700

    Code for Section 6 47 initialization

---
+++ 06/47-initialization/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Carl"
+	// var name = "Carl"
+	name := "Carl" // Short declaration
+
+	// var isScientist bool = true
+	// var isScientist = true
+	isScientist := true // Short declaration
+
+	// var age int = 62
+	// var age = 62
+	age := 62 // Short declaration
+
+	// var degree float64 = 5.
+	// var degree = 5.
+	degree := 5. // Short declaration
+
+	fmt.Println(name, isScientist, age, degree)
+}
