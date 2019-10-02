b1108ee0f1478870f74f7080df6f7b4d560564b0
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-10-01 22:03:32 -0700

    Code for Section 6 57 Ex 10

---
+++ 06/57-exercise/10-discard/main.go
@@ -0,0 +1,35 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import (
+	"fmt"
+	"path"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Discard The File
+//
+//  1. Print only the directory using `path.Split`
+//
+//  2. Discard the file part
+//
+// RESTRICTION
+//  Use short declaration
+//
+// EXPECTED OUTPUT
+//  secret/
+// ---------------------------------------------------------
+
+func main() {
+	// UNCOMMENT THE CODE BELOW:
+
+	d, _ := path.Split("secret/file.txt")
+
+	fmt.Println(d)
+}
